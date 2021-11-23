5571feb35d251b9d05b8a99b724b5e1ed9a8f13d
Mateusz Malec <m.mateusz2000@wp.pl>
2021-11-23 12:33:23 +0100

    feat: add redirect to home from '/ordering' when cart is empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Routes, Route } from 'react-router';
+import { Navigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import { useAppSelector } from './hooks/useAppStore';
 import Cart from './pages/Cart/Cart';
 import Home from './pages/Home/Home';
 import OrderForm from './pages/OrderForm/OrderForm';
 
 const App = () => {
+    const cartIsNotEmpty = useAppSelector(state => state.cartStore.cart).length;
+
     return (
         <Layout>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/cart" element={<Cart />} />
-                <Route path="/ordering" element={<OrderForm />} />
+                <Route
+                    path="/ordering"
+                    element={!!cartIsNotEmpty ? <OrderForm /> : <Navigate to="/" />}
+                />
             </Routes>
         </Layout>
     );
